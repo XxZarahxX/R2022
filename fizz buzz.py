e161847d39ce92f7a79378e314511dcf3436f142
XxZarahxX <93954064+XxZarahxX@users.noreply.github.com>
2022-01-27 17:41:53 +0100

    Update fizz buzz.py

--- fizz buzz.py
+++ fizz buzz.py
@@ -1,10 +1,12 @@
 So the first thing we are going to need is 100 numbers:
 
 for i in range(100):
-    print(i+1)# (i+1) Should print 1 to 100 inclusive (i) by itself would print from 0 to 99. # Perhaps better, you could also write it as range (1,101) and avoid adding the 1, so that's what we will use:for i in range(1,100):
+    print(i+1)# (i+1) Should print 1 to 100 inclusive (i) by itself would print from 0 to 99. # Perhaps better, you could also write it as range (1,101) 
+    and avoid adding the 1, so that's what we will use:for i in range(1,100):
     print(i)
 
-⚠️ You might have noticed that I am not using the words fizz and buzz yet, this is intentional, we want to make things as explicit and clear as possible while we are investigating solutions.
+⚠️ You might have noticed that I am not using the words fizz and buzz yet, this is intentional, we want to make things as explicit 
+    and clear as possible while we are investigating solutions.
 
 The trick here is to use the modulo operator which according to the documentation :
 
@@ -19,6 +21,20 @@ for i in range(1, 100):
         print ( str(i) + ' is a multiple of 5')
     else:
         print ( str(i) )# PARTIAL OUTPUT:8
+        
+Another way we could approach this, as well as very many problems with iteration is by using the standard library tool, “ Itertools.” 
+This will create a loop with better performance than most other typical methods of iteration. Itertools can be thought of as an iteration library that 
+is built to mirror several other extremely performant libraries from other languages, except using Pythonic methods for solving problems.
+
+Itertools will need to be imported, however, it is in the standard library. This means Pip won’t be necessary, 
+but itertools is still considered a project dependency. We are going to utilize three different methods from this module:
+
+    cycle() — This function takes a basic data-type and creates an iterator out of it. 
+               This function is about as useful as it sounds, and certainly makes building custom iterators incredibly easy in Python.
+    count() — Count is another generator that basically iterates a range. 
+               This iterator is often called an “ infinite iterator,” this basically means that the count() function could essentially loop on and on forever.
+    islice() — The islice function is short for “ iteration slice.” We can use this iterator to cut out particular elements in a data structure and iterate them.
+    
 9 is a multiple of 3
 10 is a multiple of 5
 11
@@ -40,7 +56,8 @@ for i in range(1, 100):
 45 is Multiple of 3 AND 5
 46...
 
-So we seem to have everything we need, the last thing to do is check for all the cases ( 4 if you count them ) and finally replace our explicit readouts with fizz,buzz and fizzbuzz:
+So we seem to have everything we need, the last thing to do is check for all the cases ( 4 if you count them ) 
+and finally replace our explicit readouts with fizz,buzz and fizzbuzz:
 
 for i in range(1, 101):
     if i % 3 == 0 and i % 5 == 0:
@@ -57,7 +74,8 @@ for i in range(1, 101):
 19
 20 is a multiple of 5...Note that you could also try for a modulo of 15 test since this is the earliest number divisible by 3 and 5:if i % 15== 0:
 
-⚠️ Note where the conditional for both cases is placed ( First if ), if you were to place it somewhere else it wouldn’t work because one of the other conditionals would trigger first, that’s also why we are still printing explicit readouts, it is easier to catch mistakes.
+⚠️ Note where the conditional for both cases is placed ( First if ), if you were to place it somewhere else it wouldn’t work 
+because one of the other conditionals would trigger first, that’s also why we are still printing explicit readouts, it is easier to catch mistakes.
     
   
 #x#x#x#x#x#x#x#x#x#x#x#x##x#x#x#x#x#x#x#x#x#x##
